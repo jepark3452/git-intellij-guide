d61aef43c81b5ba5cbe6eb909e649a8a92fe8952
JOOIK PARK <jepark3452@gmail.com>
2018-10-03 17:07:33 +0900

    chap1.lineedit

--- src/main/java/com/jepark/inflearn/chap1/lineedit/LineCopy.java
+++ src/main/java/com/jepark/inflearn/chap1/lineedit/LineCopy.java
@@ -2,6 +2,11 @@ package com.jepark.inflearn.chap1.lineedit;
 
 public class LineCopy {
 
+    public static void main(String[] args) {
+        LineCopy lineCopy = new LineCopy();
+        lineCopy.copyLine();
+    }
+
     public void copyLine() {
         System.out.println("현재 줄을 복사합니다.");
     }
